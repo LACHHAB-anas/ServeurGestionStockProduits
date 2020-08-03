043c72fd9825766689dee01f644791858d81d92e
anas lachhab <anaslachhab655@gmail.com>
2020-08-03 17:33:35 +0100

    first commit

---
+++ src/test/java/ma/ac/emi/demo/ServeurGestionStockProduitsApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ac.emi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurGestionStockProduitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
